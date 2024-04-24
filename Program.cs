6e75f91101299900b4ee08dde3ef78129c106c30
Shiny Gyrodos <gibbs2kids@gmail.com>
2024-04-24 10:26:13 -0500

    No substantial changes.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Card.CreateDeck();
+            Card.Draw(5);
+            
+            foreach (Card card in Card.hand)
+            {
+                Console.WriteLine($"{card.value} {card.suite} {card.title}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
